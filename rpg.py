2b27f8ffce8900541661e5df66b25d3254d6cd73
Workstudy <isws@salina.k-state.edu>
2019-09-16 15:33:48 -0500

    Working on Button Layout. Can't get some buttons to Disappear.

--- rpg.py
+++ rpg.py
@@ -22,6 +22,7 @@ class Presser(Frame):#Button Constructor
         Frame.__init__(self,master)
         self.grid()
         self.create_widgets()
+        self.create_action_widgets()
 
     def create_widgets(self):
         self.up_button=Button(self, #Up Button
@@ -29,28 +30,36 @@ class Presser(Frame):#Button Constructor
                               width=5,
                               height=1)
         self.up_button.grid(row=0,
-                            column=0)
+                            column=0,
+                            padx=2,
+                            pady=2)
 
         self.down_button=Button(self, #Down Button
                                 text="Down",
                                 width=5,
                                 height=1)
         self.down_button.grid(row=0,
-                              column=1)
+                              column=1,
+                              padx=2,
+                              pady=2)
 
         self.left_button=Button(self, #Left Button
                                 text="Left",
                                 width=5,
                                 height=1)
         self.left_button.grid(row=1,
-                              column=0)
+                              column=0,
+                              padx=2,
+                              pady=2)
 
         self.right_button=Button(self, #Right Button
                                  text="Right",
                                  width=5,
                                  height=1)
         self.right_button.grid(row=1,
-                               column=1)
+                               column=1,
+                               padx=2,
+                               pady=2)
 
         self.status=Text(self, #Output Box
                          wrap=WORD,
@@ -58,7 +67,69 @@ class Presser(Frame):#Button Constructor
                          width=20)
         self.status.grid(row=0,
                          column=2,
-                         rowspan=2)
+                         rowspan=2,
+                         padx=2,
+                         pady=2)
+
+        self.inventory_button=Button(self,
+                                     text="Inv.",
+                                     width=5,
+                                     height=1)
+        self.inventory_button.grid(row=2,
+                                   column=0,
+                                   padx=2,
+                                   pady=2)
+
+    def create_action_widgets(self):
+        self.action_button=Button(self,
+                                  text="Actions",
+                                  width=20,
+                                  height=1,
+                                  command=self.place_subaction_widgets)
+        self.action_button.grid(row=2,
+                                column=2,
+                                columnspan=2,
+                                padx=2,
+                                pady=2)
+
+        self.search_items=Button(self,
+                                 text="Search4Items",
+                                 width=10,
+                                 height=1)
+        self.pickup_item=Button(self,
+                                text="PickupItem",
+                                width=10,
+                                height=1)
+        self.back_button=Button(self,
+                                text="Back",
+                                width=5,
+                                height=1,
+                                command=self.create_action_widgets)
+
+        self.search_items.grid_remove()
+        self.pickup_item.grid_remove()
+        self.back_button.grid_remove()
+            
+    def place_subaction_widgets(self):
+        self.search_items.grid(row=2,
+                               column=2,
+                               padx=2,
+                               pady=2,
+                               sticky=W)
+
+        self.pickup_item.grid(row=2,
+                              column=2,
+                              padx=2,
+                              pady=2,
+                              sticky=E)
+
+        self.back_button.grid(row=2,
+                              column=1,
+                              padx=2,
+                              pady=2)
+        
+        self.action_button.grid_remove()
+
 
 ##zone_one={
 ##        'items':['computer','tablet','heiroglyphics','king tut'],
