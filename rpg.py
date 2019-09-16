86e2b6ce65c441f77850ac82c20f2bdc76c65b48
Workstudy <isws@salina.k-state.edu>
2019-09-16 11:09:06 -0500

    Added Buttons.

--- rpg.py
+++ rpg.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from Tkinter import *
 
-import pandas as pd
+##import pandas as pd
 
 #Creates the Window
 root=Tk()
@@ -24,28 +24,55 @@ class Presser(Frame):#Button Constructor
         self.create_widgets()
 
     def create_widgets(self):
-        self.button=Button(self,
-                           text="Doggo")
-        self.button.grid(row=1,
-                         column=0)
+        self.up_button=Button(self, #Up Button
+                              text="Up",
+                              width=5,
+                              height=1)
+        self.up_button.grid(row=0,
+                            column=0)
 
-zone_one={
-        'items':['computer','tablet','heiroglyphics','king tut'],
-        'control':['up','down','left','right']}
+        self.down_button=Button(self, #Down Button
+                                text="Down",
+                                width=5,
+                                height=1)
+        self.down_button.grid(row=0,
+                              column=1)
 
-zone_one=pd.DataFrame(zone_one,columns=['items','control'])
+        self.left_button=Button(self, #Left Button
+                                text="Left",
+                                width=5,
+                                height=1)
+        self.left_button.grid(row=1,
+                              column=0)
 
-zone_two={
-        'items':['dog','cat','sword','god'],
-        'control':['up','down','left','right']}
+        self.right_button=Button(self, #Right Button
+                                 text="Right",
+                                 width=5,
+                                 height=1)
+        self.right_button.grid(row=1,
+                               column=1)
+                                
 
-zone_two=pd.DataFrame(zone_two,columns=['items','control'])
+##zone_one={
+##        'items':['computer','tablet','heiroglyphics','king tut'],
+##        'control':['up','down','left','right']}
+##
+##zone_one=pd.DataFrame(zone_one,columns=['items','control'])
+##
+##zone_two={
+##        'items':['dog','cat','sword','god'],
+##        'control':['up','down','left','right']}
+##
+##zone_two=pd.DataFrame(zone_two,columns=['items','control'])
+##
+##frames=[zone_one,zone_two]
+##
+##zone_control_map=pd.concat(frames,keys=['zone_one','zone_two'])
+##
+##print(zone_control_map)
 
-frames=[zone_one,zone_two]
-
-zone_control_map=pd.concat(frames,keys=['zone_one','zone_two'])
-
-print(zone_control_map)
+app=Presser(root)
+root.mainloop()
 
 """
 dog=input()
