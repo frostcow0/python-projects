16abda86f6a242077925a9652e67f9364de09c6a
Jon Martin <gametime785@gmail.com>
2021-03-23 21:49:32 -0500

    New project.

---
+++ covid.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 21:43:50 2021
+
+@author: gamet
+"""
+from sklearn.tree import RandomForestRegressor
+from sklearn.metrics import mean_absolute_percentage_error as mape
+
+
+
