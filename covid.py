9ca7caf7f4c8fbce36660086fcafa9b35be66bc9
Jon Martin <gametime785@gmail.com>
2021-04-21 20:41:06 -0500

    Transformed iso_codes to numerical values. Got an evaluation output. Need to test for output accuracy.

--- covid.py
+++ covid.py
@@ -7,6 +7,7 @@ Created on Tue Mar 23 21:43:50 2021
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error as mae
 from sklearn.model_selection import train_test_split as tts
+from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 
 data=pd.read_csv(r'C:\Users\gamet\OneDrive\Documents\Data\owid-covid-data.csv')
@@ -17,6 +18,18 @@ data=pd.read_csv(r'C:\Users\gamet\OneDrive\Documents\Data\owid-covid-data.csv')
 Data from the features needs cleaned. Yikes.
 """
 
+#create initial DF
+iso_codes=data.iso_code.unique()
+iso_df=pd.DataFrame(iso_codes,columns=['ISO_Codes'])
+#create labelencoder instance
+labelencoder=LabelEncoder()
+#assign numerical values and store in new column
+iso_df['ISO_Codes_Cat']=labelencoder.fit_transform(iso_df['ISO_Codes'])
+
+#This would hopefully finish the categorical->numerical change for iso codes
+data['iso_code']=iso_df['ISO_Codes_Cat']
+
+data=data.fillna(0) # Trying to fill in the NAN spots with 0
 
 # =============================================================================
 # print('='*40)
@@ -25,7 +38,7 @@ Data from the features needs cleaned. Yikes.
 #     print('-'*40)
 # =============================================================================
     
-features=['iso_code','location','total_cases','new_cases','icu_patients','new_tests','total_tests',
+features=['iso_code','total_cases','new_cases','icu_patients','new_tests','total_tests',
           'positive_rate']
 
 y=data.extreme_poverty
@@ -38,6 +51,6 @@ model=RandomForestRegressor(random_state=23)
 model.fit(train_X,train_y)
 
 val_predictions=model.predict(val_X)
-val_mae=mean_absolute_error(val_predictions,val_y)
+val_mae=mae(val_predictions,val_y)
 
 print('Validation MAE: {:,.2f}'.format(val_mae))
