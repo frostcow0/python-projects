fbc38e1ef46f4b6885a3ad343a190e527eb4e071
Jonathan Martin <Jonathan.Martin@metronetinc.com>
2021-07-26 10:25:32 -0500

    fixing queries.

--- query_compare.py
+++ query_compare.py
@@ -21,7 +21,8 @@ select distinct
     to_char(min(payment.trans_dttm), 'YYYY-MM-DD') as min_payment_dt,
     to_char(max(payment.trans_dttm), 'YYYY-MM-DD') as max_payment_dt, 
     sum(payment.amt) as payment_sum_amt, count(payment.amt) as count_of_payment,
-    invoice.trans_nm as trans_nm
+    round(sum(payment.amt)/count(payment.amt), 2) as avg_payment,
+    listagg(invoice.trans_nm, '; ') as transactions
 from
     (
         select
@@ -41,7 +42,7 @@ from
             and t.trans_typ in nm.trans_typ
         group by trans_nm, sub_id, trans_cls
     ) invoice,
-    lateral(
+    (
         select
             trans_dttm, amt, sub_id
         from wasabi.v_trans
@@ -51,11 +52,11 @@ from
 where payment.trans_dttm >= invoice.trans_dttm
 and invoice.sub_id = s.sub_id
 and payment.sub_id = s.sub_id
-group by s.sub_id, s.sub_nm, invoice.trans_dttm, invoice.amt, invoice.trans_nm
+group by s.sub_id, s.sub_nm, invoice.amt, invoice.trans_dttm, invoice.trans_nm
     '''
     df = pd.read_sql(query, connection)
     print('\t - Read sql.')
-    filename = 'Billed_Equipment_6_mo_1.xlsx'
+    filename = 'Billed_Equipment_Paid_6mo.xlsx'
     sheetname = 'Subscribers & Equipment'
     writer = pd.ExcelWriter(filename, engine = 'xlsxwriter')
     df.to_excel(writer, sheet_name = sheetname, header = False, index = False)
@@ -66,7 +67,7 @@ group by s.sub_id, s.sub_nm, invoice.trans_dttm, invoice.amt, invoice.trans_nm
     writer.save()
     print('\t - Finished.')
 
-def trial_2(login_info):
+def trial_2(login_info): # Attempts to merge dataframes with two queries
     dsn_tns = cx_Oracle.makedsn('ora-tns-qcc1.in.qservco.com', 1521, service_name = 'qcc1' )
     connection = cx_Oracle.connect(
         user = login_info[0],
@@ -122,7 +123,7 @@ and sub_id = :sub_id
     print('\t - Merged Dataframes.')
     print(f'\t - DF Info:\n{df.info}')
     print(f'\t - DF Head:\n{df.head}')
-    filename = 'Billed_Equipment_6_mo_2.xlsx'
+    filename = 'Billed_Equipment_Paid_6mo.xlsx'
     sheetname = 'Subscribers & Equipment'
     writer = pd.ExcelWriter(filename, engine = 'xlsxwriter')
     df.to_excel(writer, sheet_name = sheetname, header = False, index = False)
@@ -133,10 +134,69 @@ and sub_id = :sub_id
     writer.save()
     print('\t - Finished.')
 
+def trial_3(login_info): # Looks for charges without payments
+    dsn_tns = cx_Oracle.makedsn('ora-tns-qcc1.in.qservco.com', 1521, service_name = 'qcc1' )
+    connection = cx_Oracle.connect(
+        user = login_info[0],
+        password = login_info[1],
+        dsn = dsn_tns)
+    print('='*60)
+    print('\t - Connection established.')
+    query = '''
+select distinct
+    s.sub_id, s.sub_nm, to_char(invoice.trans_dttm, 'YYYY-MM-DD') as invoice_dt,
+    invoice.amt as invoice_sum_amt,
+    listagg(invoice.trans_nm, '; ') as transactions
+from
+    (
+        select
+            max(trans_dttm) as trans_dttm, sum(amt) as amt,
+            trans_nm, sub_id
+        from wasabi.v_trans t,
+        (
+            select
+                trans_typ, trans_nm
+            from wasabi.v_trans_typ
+            where lower(trans_nm) like '%equip%nrc%'
+        ) nm
+        where t.trans_cls = 'R'
+            and t.amt > 0
+            and (t.trans_stat = 'I' or t.trans_stat = 'C')
+            and t.trans_dttm >= add_months(trunc(sysdate), -6)
+            and t.trans_typ in nm.trans_typ
+        group by trans_nm, sub_id, trans_cls
+    ) invoice, wasabi.v_sub s
+where invoice.sub_id = s.sub_id
+and not exists (
+        select
+            null
+        from wasabi.v_trans
+        where trans_cls = 'P'
+            and (trans_stat = 'I' or trans_stat = 'C')
+            and sub_id = invoice.sub_id
+            and trans_dttm >= invoice.trans_dttm
+    )
+group by s.sub_id, s.sub_nm, invoice.trans_dttm, invoice.amt, invoice.trans_nm
+    '''
+    df = pd.read_sql(query, connection)
+    print('\t - Read sql.')
+    filename = 'Billed_Equipment_Not_Paid_6mo.xlsx'
+    sheetname = 'Subscribers & Equipment'
+    writer = pd.ExcelWriter(filename, engine = 'xlsxwriter')
+    df.to_excel(writer, sheet_name = sheetname, header = False, index = False)
+    worksheet = writer.sheets[sheetname]
+    (max_row, max_col) = df.shape
+    headers = [{'header': header } for header in df.columns]
+    worksheet.add_table(0, 0, max_row - 1, max_col - 1, {'columns': headers, 'style': 'Table Style Dark 5'})
+    writer.save()
+    print('\t - Finished.')
+
 t1 = timeit.Timer('trial_1(login_info)', 'from __main__ import trial_1, login_info').timeit(number=1)
 #t2 = timeit.Timer('trial_2(login_info)', 'from __main__ import trial_2, login_info').timeit(number=1)
+t3 = timeit.Timer('trial_3(login_info)', 'from __main__ import trial_3, login_info').timeit(number=1)
 
-print('-'*60)
+print('+'*60)
 print(f'\t - Trial 1 took {t1} seconds.')
 # print(f'\t - Trial 2 took {t2} seconds.')
+print(f'\t - Trial 3 took {t3} seconds.')
 print('='*60)
