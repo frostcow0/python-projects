623c6c9da625ac2225bf2fe446d35c0c8a0cf589
Jon Martin <gametime785@gmail.com>
2021-09-07 21:11:21 -0500

    updates

--- webscraping/html_parsing.py
+++ webscraping/html_parsing.py
@@ -1,11 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+import pandas as pd
 
-url     = 'https://en.wikipedia.org/wiki/Chinampa'
+# url     = 'https://en.wikipedia.org/wiki/Chinampa'
+url     = 'https://en.wikipedia.org/wiki/Long_Walls'
 text    = requests.get(url).text
 soup    = BeautifulSoup(text, 'html.parser')
 
-# print(soup.get_text())
+def parsinTime(reflist):
+    data = {
+        'Author': [],
+        'The rest :)': []
+    }
+    for ref in reflist.find_all('li'): # Each reference
+        reference = re.sub(r'^[\^\sa-z]+', '', ref.text)
+        reference = reference.replace('.\n', '')
+        splitRef = reference.split('. ')
+        data['Author'].append(splitRef[0])
+        try:
+            data['The rest :)'].append(splitRef[1])
+        except:
+            data['The rest :)'].append('')
+    df = pd.DataFrame(data)
+    print(df)
 
 if __name__ == '__main__':
     content = soup.find(id = 'content')
@@ -16,12 +34,17 @@ if __name__ == '__main__':
             if contentText:
                 reflist = contentText.find('div', {'class': 'reflist reflist-columns references-column-width reflist-columns-2'})
                 if reflist:
-                    for ref in reflist.find_all('li'):
-                        print(ref)
-                        print('-'*40)
-
+                    parsinTime(reflist)
                 else:
-                    print("No reflist")
+                    parser = contentText.find(id = 'mw-parser-output')
+                    if parser:
+                        reflist = contentText.find('div', {'class': 'reflist'})
+                        if reflist:
+                            parsinTime(reflist)
+                        else:
+                            print("No reflist")
+                    else:
+                        print("No reflist or parser")
             else:
                 print("No contentText")
         else:
