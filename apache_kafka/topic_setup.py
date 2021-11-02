d055ca871929f0945084233801000933906a1812
Jon Martin <gametime785@gmail.com>
2021-11-02 16:51:04 -0500

    Making sure the connect topics are config'd right. Also a lot of restructuring. Looks a lot better.

--- apache_kafka/topic_setup.py
+++ apache_kafka/topic_setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka import admin
+from confluent_kafka.admin import AdminClient, ConfigResource, NewTopic, TopicMetadata
 
 from utils.parse_command_line_args import topic_parse_command_line_args
 
@@ -8,73 +9,81 @@ from utils.parse_command_line_args import topic_parse_command_line_args
 # Global var for creating topics & checking our main topic
 PARTITIONS = 3
 
-def check_if_topic_exists(adminclient:AdminClient, topic:str) -> bool:
-    """ **DEPRECATED - Check if topic exists. Returns True or False. """
+def check_topic_config(adminclient:AdminClient, topic:TopicMetadata) -> None:
+    """ Checks topic's config """
+    print(f'\t-Pulling config for topic {topic.topic}. . .')
+    resource = ConfigResource(2, topic.topic)
+    if '_connect' in topic.topic: # Specific to Kafka Connect's topic needs
+        futures = adminclient.describe_configs(
+            [resource],
+            request_timeout = 20)
+        try:
+            # print(futures) # Should be None`
+            print(futures[resource].result())
+        except Exception as error:
+            print('\t-Error getting config for topic '
+            f'{topic.topic}: ',error)
+    partitions = len(topic.partitions)
+    if partitions != PARTITIONS:
+        print(f'\t-Insufficient partitions ({partitions})... recreating topic.')
 
-    metadata = adminclient.list_topics(timeout = 10)
-    # md.topics.values is TopicMetadata
-    # md.topics.keys is the topic name
-
-    print(f'\t-Checking if topic {topic} exists. . .')
-    for topic_metadata in iter(metadata.topics.values()):
-        if topic_metadata.topic[0] == '_':
-            continue
-        if topic_metadata.error is not None:
-            print(f'\t-Error with topic {topic_metadata.topic}: {topic_metadata.error}')
-        else:
-            print(f'\t-Found topic {topic_metadata} with '
-                f'{len(topic_metadata.partitions)} partitions.')
-            if len(topic_metadata.partitions) != PARTITIONS:
-                print('\t-Insufficient partitions... recreating topic.')
-                print(f'\t---Current Partitions: {len(topic_metadata.partitions)}')
-                del_topic(adminclient, [topic_metadata.topic])
-                return False
-    # don't have to specify md.topics.keys, 'in' for dicts always checks keys
-    return True if topic in metadata.topics else False
-
-def print_topics_nice(topics):
+def print_indent_nice(topics:list) -> None:
+    """ Decorator for printing topic names """
     for topic in topics:
         print(f'\t--- {topic}')
 
-def check_if_topics_exist(adminclient:AdminClient, topics:list) -> list:
+def check_if_topics_exist(topics:list, all_topics:list) -> list:
     """ Check if topics exist. Returns list of topics that don't exist. """
-
-    metadata = adminclient.list_topics(timeout = 10)
-    # md.topics.values is TopicMetadata
-    # md.topics.keys is the topic name
-
     print(f'\t-Checking if topics {topics} exist. . .')
-    found = [topic for topic in topics if topic in metadata.topics]
+    found = [topic for topic in topics if topic in all_topics]
     print('\t-Found these topics:')
-    print_topics_nice(found)
-    
-    return list(set(topics) - set(found)) # Should return topics that weren't found
+    print_indent_nice(found)
+    return list(set(topics) - set(found)) # Returns topics that weren't found
 
 def del_topic(adminclient:AdminClient, topics:list) -> None:
     """ Delete topics """
-    exists = map(check_if_topic_exists, topics)
-    if False not in exists:
-        print('** New Functionality from topic_setup.py: exists true')
     futures = adminclient.delete_topics(topics, operation_timeout = 30)
-
     for topic, future in futures.items(): # tpc is topic b/c topic is in use
         try:
             future.result()
             print(f'\t-Deleted topic {topic}')
-        except Exception as e:
-            print(f'\t-Error deleting topic {topic}: ',e)
+        except Exception as error:
+            print(f'\t-Error deleting topic {topic}: ',error)
+
+def check_topics(adminclient:AdminClient, topics:list) -> bool:
+    """ Creates topics if need be & checks the topic configs """
+    metadata = adminclient.list_topics(timeout = 10)
+    # md.topics.values is TopicMetadata
+    # md.topics.keys is the topic name
+
+    # Check if topics exist
+    topics_to_be_created = check_if_topics_exist(
+        topics,
+        metadata.topics.keys())
+    if topics_to_be_created:
+        create_topics(adminclient, topics_to_be_created)
+
+    # Check topics configs
+    for topic in topics:
+        topic_metadata = metadata.topics[topic]
+        check_topic_config(adminclient, topic_metadata)
+        # We get the dict of the current config, now we need to check
+        # current config against what we need for connect and then
+        # set the new config, and hope that the other defaults are okay :)
+
+    return True
+
+
 
 def create_topics(adminclient:AdminClient, topics:list) -> None:
     """ Create topics """
     new_topics = [NewTopic(topic,
         num_partitions=PARTITIONS,
         replication_factor=1) for topic in topics]
-
     print('\t-Creating topics. . .')
     # Call create_topics to asynchronously create topics, a dict
     # of <topic,future> is returned.
     futures = adminclient.create_topics(new_topics)
-
     # Wait for operation to finish.
     # Timeouts are preferably controlled by passing request_timeout=15.0
     # to the create_topics() call.
@@ -83,12 +92,11 @@ def create_topics(adminclient:AdminClient, topics:list) -> None:
         try:
             future.result()  # The result itself is None
             print(f"\t--- Topic {topic} created")
-        except Exception as e:
-            print(f"\t--- Failed to create topic {topic}: {e}")
+        except Exception as error:
+            print(f"\t--- Failed to create topic {topic}: {error}")
 
 def main(args) -> None:
     """ Quality Control of our Topics """
-    topics_to_be_created: list
     admin_config = {
         'bootstrap.servers': args.bootstrap_servers
     }
@@ -96,19 +104,10 @@ def main(args) -> None:
     topics = args.topic.rstrip().split(',')
     # del_topic(a, [topic])
 
-    topics_to_be_created = check_if_topics_exist(adminclient, topics)
-    if len(topics_to_be_created) > 0:
-        print('\t-Topics that don\'t exist yet:')
-        print_topics_nice(topics_to_be_created)
-
-        create_topics(adminclient, topics_to_be_created)
-    print('\t-Ready to go!')
-
-    # if check_if_topic_exists(adminclient, topic):
-    #     print(f'\t-Topic {topic} exists.')
-    # else:
-    #     print(f'\t-Topic {topic} doesn\'t exist yet!')
-    #     create_topics(adminclient, [topic])
+    if check_topics(adminclient, topics):
+        print('\t-Ready to go!')
+    else:
+        print('\t-Something went wrong. . .')
 
 if __name__ == '__main__':
     main(topic_parse_command_line_args())
