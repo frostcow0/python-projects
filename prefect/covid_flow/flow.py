ad37ab47a87294cef30efe0bd88e0967a6bd2457
Jon Martin <gamet@gmail.com>
2021-12-02 21:14:14 -0600

    Prepping for MLOps. Extra print tasks. Data cleaning tasks. Data splitting tasks.

--- prefect/covid_flow/flow.py
+++ prefect/covid_flow/flow.py
@@ -2,7 +2,7 @@
 import io
 import json
 import urllib.request as rq
-from typing import Any
+from typing import Any, Dict
 from datetime import timedelta, date
 
 # Third Party
@@ -13,11 +13,13 @@ from prefect import task, Flow
 from prefect.run_configs import LocalRun
 from prefect.core.parameter import Parameter
 # from prefect.engine.results import LocalResult
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 
 DEFAULT_COUNTRY = "USA"
-COVID_DATA_URL = "https://covid.ourworldindata.org/data/latest/owid-covid-latest.json"
-# https://covid.ourworldindata.org/data/owid-covid-data.json
+# COVID_DATA_URL = "https://covid.ourworldindata.org/data/latest/owid-covid-latest.json"
+COVID_DATA_URL = "https://covid.ourworldindata.org/data/owid-covid-data.json"
 FLOW_NAME = "Covid analysis workflow"
 
 
@@ -43,10 +45,38 @@ def print_data(data:Any) -> None:
     print(data)
 
 @task
-def save_filtered_covid_data(data:pd.DataFrame) -> None:
-    """Saves dataframe to csv"""
+def print_columns(data:pd.DataFrame) -> None:
+    """Prints columns of dataframe"""
+    print([column for column in data.columns])
+
+@task
+def print_head(data:pd.DataFrame) -> None:
+    """Print the head of a dataframe"""
+    print(data.head())
+
+@task
+def save_data(data:pd.DataFrame, type:str, range:str) -> None:
+    """Saves dataframe to csv. Ex. of type is filtered. Ex. of range is latest."""
     today = date.today()
-    data.to_csv(f'../data/filtered-covid-latest-{today}.csv', index=False)
+    data.to_csv(f'./data/{type}-covid-latest-{today}.csv', index=False)
+
+@task
+def extract_full_country_data(data:pd.DataFrame) -> pd.DataFrame:
+    df = pd.DataFrame.from_dict(data.data)
+    return df
+
+@task
+def extract_label_column(data:pd.DataFrame, column:str) -> pd.Series:
+    return data.copy()[column]
+
+@task
+def clean_NaN(data:pd.DataFrame) -> pd.DataFrame:
+    df = data.copy().fillna(0)
+    return df
+
+@task
+def split_data(data:Dict) -> Dict:
+    pass
 
 def create_flow() -> Flow:
     """Creates and returns flow object"""
@@ -56,10 +86,18 @@ def create_flow() -> Flow:
 
         covid_df = extract_covid_data()
         filtered_covid_df = filter_data(covid_df, country)
+        print_head(filtered_covid_df)
 
-        print_data(filtered_covid_df)
+        # Only for whole data, not latest
+        full_df = extract_full_country_data(filtered_covid_df)
+        # print_columns(full_df)
 
-        save_filtered_covid_data(filtered_covid_df)
+        label = extract_label_column(full_df, 'new_cases')
+        label = clean_NaN(label)
+        print_head(label)
+
+
+        # save_data(filtered_covid_df, 'filtered', 'latest')
 
     return flow
 
