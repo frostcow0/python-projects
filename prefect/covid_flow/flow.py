c0a8379764e6761c2c791c78b1c4ac00dad6792f
Jon Martin <gamet@gmail.com>
2021-12-03 15:16:49 -0600

    Cleaned and split features and label. Optimize for features using f_regressor. Attempting to scale data.

--- prefect/covid_flow/flow.py
+++ prefect/covid_flow/flow.py
@@ -15,8 +15,19 @@ from prefect.core.parameter import Parameter
 # from prefect.engine.results import LocalResult
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.neural_network import MLPRegressor
 
 
+DROP_COLUMNS = [
+    'date',
+    'new_cases',
+    'new_cases_per_million',
+    'new_cases_smoothed',
+    'new_cases_smoothed_per_million',
+    'tests_units'
+]
+RANDOM_STATE = 2
 DEFAULT_COUNTRY = "USA"
 # COVID_DATA_URL = "https://covid.ourworldindata.org/data/latest/owid-covid-latest.json"
 COVID_DATA_URL = "https://covid.ourworldindata.org/data/owid-covid-data.json"
@@ -55,19 +66,40 @@ def print_head(data:pd.DataFrame) -> None:
     print(data.head())
 
 @task
+def print_info(data:pd.DataFrame) -> None:
+    """Print the info method of a dataframe"""
+    print(data.info())
+
+@task
 def save_data(data:pd.DataFrame, type:str, range:str) -> None:
     """Saves dataframe to csv. Ex. of type is filtered. Ex. of range is latest."""
     today = date.today()
-    data.to_csv(f'./data/{type}-covid-latest-{today}.csv', index=False)
+    data.to_csv(f'./data/{type}-covid-{range}-{today}.csv', index=False)
 
 @task
 def extract_full_country_data(data:pd.DataFrame) -> pd.DataFrame:
+    """Creates a dataframe from the data dictionary nested in the covid data"""
     df = pd.DataFrame.from_dict(data.data)
     return df
 
 @task
 def extract_label_column(data:pd.DataFrame, column:str) -> pd.Series:
+    """Create a copy of the column and return it as a Series"""
     return data.copy()[column]
+
+@task
+def optimize_feature_columns(X:pd.DataFrame, num_features:int, y:pd.Series) -> pd.DataFrame:
+    """Create a copy of the columns and return them as a dataframe"""
+    feature_selection = SelectKBest(score_func=f_regression, k=num_features)
+    X_selected = feature_selection.fit_transform(X.copy(), y)
+    X_selected = pd.DataFrame(X_selected)
+    return X_selected
+
+@task
+def remove_overfit_columns(data:pd.DataFrame, columns:list) -> pd.DataFrame:
+    """Removes columns that might cause overfit, such as the per million and smoothed versions of our label"""
+    data = data.copy().drop(columns = columns)
+    return data
 
 @task
 def clean_NaN(data:pd.DataFrame) -> pd.DataFrame:
@@ -75,8 +107,34 @@ def clean_NaN(data:pd.DataFrame) -> pd.DataFrame:
     return df
 
 @task
-def split_data(data:Dict) -> Dict:
-    pass
+def split_data(X:pd.DataFrame, y:pd.Series) -> Dict:
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=RANDOM_STATE
+    )
+    return {
+        "X_train": X_train,
+        "X_test": X_test,
+        "y_train": y_train,
+        "y_test": y_test}
+
+@task
+def scale_data(data:Dict) -> Dict:
+    """Uses StandardScaler to scale the data"""
+    scaler = StandardScaler()
+    scaler.fit(data['X_train'])
+    for k, v in data.items():
+        data[k] = scaler.transform(v)
+    return data
+
+@task
+def train_model(data:Dict) -> MLPRegressor:
+    """Trains MLPRegressor"""
+    model = MLPRegressor(
+        random_state=RANDOM_STATE,
+        max_iter=500,
+        learning_rate_init=0.1
+    ).fit(data['X_train'], data['y_train'])
+    return model
 
 def create_flow() -> Flow:
     """Creates and returns flow object"""
@@ -86,15 +144,23 @@ def create_flow() -> Flow:
 
         covid_df = extract_covid_data()
         filtered_covid_df = filter_data(covid_df, country)
-        print_head(filtered_covid_df)
 
         # Only for whole data, not latest
         full_df = extract_full_country_data(filtered_covid_df)
-        # print_columns(full_df)
 
-        label = extract_label_column(full_df, 'new_cases')
-        label = clean_NaN(label)
-        print_head(label)
+        base_y = extract_label_column(full_df, 'new_cases')
+        cleaned_y = clean_NaN(base_y)
+        print_head(cleaned_y)
+
+        base_X = remove_overfit_columns(full_df, DROP_COLUMNS)
+        cleaned_X = clean_NaN(base_X)
+        optimal_X = optimize_feature_columns(cleaned_X, 10, cleaned_y)
+        print_head(optimal_X)
+
+        train_test_data = split_data(optimal_X, cleaned_y)
+        scaled_data = scale_data(train_test_data)
+
+        trained_model = train_model(scaled_data)
 
 
         # save_data(filtered_covid_df, 'filtered', 'latest')
