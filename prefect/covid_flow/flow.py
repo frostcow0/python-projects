3638b37b78d375cdf995e49a058bf7966a1a17f5
Jon Martin <gamet@gmail.com>
2021-12-04 21:48:55 -0600

    Model testing. Added prints. Added model training. Added GridSearchVC. Implemented Scaler. Fixed save data to save json format. Made separate extractions for different time frames. Issues with data format in MLPRegressor model.

--- prefect/covid_flow/flow.py
+++ prefect/covid_flow/flow.py
@@ -7,6 +7,7 @@ from datetime import timedelta, date
 
 # Third Party
 import pandas as pd
+import numpy as np
 import prefect
 from prefect import task, Flow
 # from prefect.executors import LocalDaskExecutor
@@ -14,7 +15,7 @@ from prefect.run_configs import LocalRun
 from prefect.core.parameter import Parameter
 # from prefect.engine.results import LocalResult
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn.neural_network import MLPRegressor
 
@@ -27,20 +28,48 @@ DROP_COLUMNS = [
     'new_cases_smoothed_per_million',
     'tests_units'
 ]
+PARAM_GRID = [
+    {
+        'activation': ['identity', 'logistic', 'tanh', 'relu'],
+        'solver': ['lbfgs', 'sgd', 'adam'],
+        'hidden_layer_sizes': [
+            (1,),(2,),(3,),(4,),(5,),(6,),(7,),
+            (8,),(9,),(10,),(11,),(12,),(13,),
+            (14,),(15,),(16,),(17,),(18,),(19,),
+            (20,),(21,)
+        ],
+        'max_iter': [600, 700, 800, 900, 1000]
+    }
+]
 RANDOM_STATE = 2
 DEFAULT_COUNTRY = "USA"
-# COVID_DATA_URL = "https://covid.ourworldindata.org/data/latest/owid-covid-latest.json"
-COVID_DATA_URL = "https://covid.ourworldindata.org/data/owid-covid-data.json"
+COVID_LATEST_DATA_URL = "https://covid.ourworldindata.org/data/latest/owid-covid-latest.json"
+COVID_WHOLE_DATA_URL = "https://covid.ourworldindata.org/data/owid-covid-data.json"
+COVID_DATA_FILEPATH = "./data/raw-covid-whole-2021-12-04.json"
 FLOW_NAME = "Covid analysis workflow"
 
-
-@task#(max_retries=3, delay=timedelta(seconds=10)) # (result=LocalResult())
-def extract_covid_data() -> pd.DataFrame:
-    """Download data via HTTP and create DataFrame"""
-    with rq.urlopen(COVID_DATA_URL) as url:
+def get_covid_df(url:str) -> pd.DataFrame:
+    """Helper function for data extraction tasks"""
+    with rq.urlopen(url) as url:
         covid_data = json.loads(url.read().decode())
         covid_df = pd.DataFrame(covid_data)
         return covid_df
+
+@task#(max_retries=3, delay=timedelta(seconds=10)) # (result=LocalResult())
+def extract_whole_covid_data() -> pd.DataFrame:
+    """Download data via HTTP and create DataFrame"""
+    return get_covid_df(COVID_WHOLE_DATA_URL)
+
+@task
+def extract_latest_covid_data() -> pd.DataFrame:
+    """Download data via HTTP and create DataFrame"""
+    return get_covid_df(COVID_LATEST_DATA_URL)
+
+@task
+def extract_covid_data_from_file() -> pd.DataFrame:
+    """Return dataframe of data from JSON"""
+    covid_df = pd.read_json(COVID_DATA_FILEPATH)
+    return covid_df
 
 @task
 def filter_data(covid_df:pd.DataFrame, country:str) -> pd.DataFrame:
@@ -74,7 +103,7 @@ def print_info(data:pd.DataFrame) -> None:
 def save_data(data:pd.DataFrame, type:str, range:str) -> None:
     """Saves dataframe to csv. Ex. of type is filtered. Ex. of range is latest."""
     today = date.today()
-    data.to_csv(f'./data/{type}-covid-{range}-{today}.csv', index=False)
+    data.to_json(f'./data/{type}-covid-{range}-{today}.json')
 
 @task
 def extract_full_country_data(data:pd.DataFrame) -> pd.DataFrame:
@@ -85,7 +114,8 @@ def extract_full_country_data(data:pd.DataFrame) -> pd.DataFrame:
 @task
 def extract_label_column(data:pd.DataFrame, column:str) -> pd.Series:
     """Create a copy of the column and return it as a Series"""
-    return data.copy()[column]
+    series = data.copy()[column].rename('')
+    return series
 
 @task
 def optimize_feature_columns(X:pd.DataFrame, num_features:int, y:pd.Series) -> pd.DataFrame:
@@ -118,23 +148,36 @@ def split_data(X:pd.DataFrame, y:pd.Series) -> Dict:
         "y_test": y_test}
 
 @task
-def scale_data(data:Dict) -> Dict:
+def scale_data(X:pd.DataFrame) -> pd.DataFrame:
     """Uses StandardScaler to scale the data"""
-    scaler = StandardScaler()
-    scaler.fit(data['X_train'])
-    for k, v in data.items():
-        data[k] = scaler.transform(v)
-    return data
+    X = StandardScaler().fit_transform(X)
+    X = pd.DataFrame(X)
+    return X
 
 @task
-def train_model(data:Dict) -> MLPRegressor:
+def check_data(data:Dict) -> None:
+    """Checks train_test dict for NaN and infinite values"""
+    for k in data:
+        print(np.any(np.isnan(data[k])))
+        print(np.all(np.isfinite(data[k])))
+    return None
+
+@task
+def grid_search(data:Dict) -> None:
+    """Uses GridSearchVC to hypertune parameters using MLPRegressor & PARAM_GRID"""
+    reg = GridSearchCV(MLPRegressor(), PARAM_GRID,
+        cv=3, scoring='neg_mean_absolute_error')
+    reg.fit(data['X_train'], data['y_train'])
+    print("Best parameters set found on development set:")
+    print(reg.best_params_)
+    return None
+
+@task
+def train_model(data:Dict) -> None:
     """Trains MLPRegressor"""
-    model = MLPRegressor(
-        random_state=RANDOM_STATE,
-        max_iter=500,
-        learning_rate_init=0.1
-    ).fit(data['X_train'], data['y_train'])
-    return model
+    reg = MLPRegressor(max_iter=1000)
+    reg.fit(data['X_train'], data['y_train'])
+    return None
 
 def create_flow() -> Flow:
     """Creates and returns flow object"""
@@ -142,28 +185,35 @@ def create_flow() -> Flow:
     with Flow(FLOW_NAME, run_config=LocalRun()) as flow:
         country = Parameter("country", default=DEFAULT_COUNTRY)
 
-        covid_df = extract_covid_data()
+        # covid_df = extract_whole_covid_data()
+        covid_df = extract_covid_data_from_file()
         filtered_covid_df = filter_data(covid_df, country)
-
+        
         # Only for whole data, not latest
         full_df = extract_full_country_data(filtered_covid_df)
 
         base_y = extract_label_column(full_df, 'new_cases')
         cleaned_y = clean_NaN(base_y)
-        print_head(cleaned_y)
+        # print_head(cleaned_y)
 
         base_X = remove_overfit_columns(full_df, DROP_COLUMNS)
         cleaned_X = clean_NaN(base_X)
         optimal_X = optimize_feature_columns(cleaned_X, 10, cleaned_y)
-        print_head(optimal_X)
+        scaled_X = scale_data(optimal_X)
+        # print_head(scaled_X)
 
-        train_test_data = split_data(optimal_X, cleaned_y)
-        scaled_data = scale_data(train_test_data)
+        train_test_data = split_data(scaled_X, cleaned_y)
+        check_data(train_test_data)
+        # check_for_infinity(train_test_data)
 
-        trained_model = train_model(scaled_data)
+        # Some issue with my data's format & type while being processed
+        # within the model.
+        # Going back to Kaggle. Maybe I'm using the wrong model?
+        # Just don't know enough yet.
+        # train_model = grid_search(train_test_data)
 
 
-        # save_data(filtered_covid_df, 'filtered', 'latest')
+        # save_data(covid_df, 'raw', 'whole')
 
     return flow
 
