51ce054bad710f7b628bce0d470ba6ea292e3776
Jon <gametime785@gmail.com>
2022-04-20 12:15:59 -0500

    Basic front-end using streamlit (awesome library)

--- ml/playlist_curator/streamlit_app.py
+++ ml/playlist_curator/streamlit_app.py
@@ -1,3 +1,24 @@
+import logging
 import streamlit as st
+import pandas as pd
+from main import run_flow
 
 
+logging.basicConfig(level=logging.INFO)
+
+def get_recommendations() -> pd.DataFrame:
+    """Calls run_flow and displays the recommended songs"""
+    recommended = run_flow()
+    st.markdown("## Songs added to your playlist!")
+    st.table(recommended)
+
+st.markdown("# Playlist Curator v0.5")
+st.write(("An app that, using your recently played songs,"
+    " creates a playlist of 20 songs tailored to you."
+))
+
+st.markdown("\n")
+submit = st.button(label="Create my playlist!", on_click=get_recommendations)
+
+st.markdown("\n")
+st.write("Made by Jon Martin, last updated 4/20/22")
